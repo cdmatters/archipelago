2bd4fd83e2d6e80dde4efdda05695bade0229a91
Eric Hambro <eric.hambro@gmail.com>
2015-04-03 23:33:38 -0400

    method for adding twirps done, needs to be request timed, but fine

--- Twirps/twirps.py
+++ Twirps/twirps.py
@@ -11,7 +11,9 @@ import os
 import tweepy_key as tk
 
 
+
 def create_twirpy_db():
+    '''Creates a database with tables for TweetData and TwirpData'''
     
     if not os.path.exists('./twirpy.db'):
         with sqlite3.connect('twirpy.db') as connection:
@@ -25,7 +27,10 @@ def create_twirpy_db():
                                                 BeenRetweeted Number, FavouriteHashtag Text, \
                                                 HashtagCount Number, OfficialId Number)')
 
+
 def authorize_twitter():
+    '''Authorizes the session for access to twitter API'''
+
     auth = tweepy.auth.OAuthHandler(tk.consumer_key, tk.consumer_secret)
     auth.set_access_token(tk.access_token, tk.access_secret)
 
@@ -36,42 +41,62 @@ def authorize_twitter():
 
 
 def return_twitter_list():
+    '''Returns a list of tuples, each containing an MPs OfficialId and Twitter handle'''
+
     with sqlite3.connect('../parl.db') as connection:
         cur = connection.cursor()
         cur.execute('SELECT OfficialId, Address FROM Addresses WHERE Type="Twitter"')
         tuplelist = cur.fetchall()
+    complete_mp_list = [(o_id, handle[20:]) for o_id, handle in tuplelist]
+    return complete_mp_list
+
+def return_skip_list():
+    '''Returns a list of tuples, each containing OfficialId and Twitter handle for 
+Twirps that have already been collected'''
+
+    with sqlite3.connect('twirpy.db') as connection:
+        cur = connection.cursor()
+        cur.execute('SELECT OfficialId, Handle FROM TwirpData')
+        tuplelist = cur.fetchall()
     return tuplelist
 
-
 def collect_twirp_data(api, handle, official_id):
-    
+    '''Feeding in the session, a handle and it's mp's official id, this queries the
+the twitter API, instantiates Twirp class with the data and populates the database 
+with that record'''
+
     twitter_user = api.get_user(screen_name=handle)
     twirp = Twirp(twitter_user, 'twitter')
     twirp.official_id = official_id
-    
+
     print twirp
     twirp.to_database()
 
-# def pull_tweets_from_user(api, handle):
-#     print handle
-#     mp = api.get_user(screen_name=handle)
-#     for status in tweepy.Cursor(api.user_timeline, id=mp.id).items(1000):
-#         tweet_parser(status)
-
-#         for mention in status.entities['user_mentions']:
-#             print i, mention['screen_name'], '\t', status.text
-    
-#     time.sleep(45)
 
 
 def monitor_calls(api):
     '''This function will return the number of requests remaining and the 
-    session expiration time, for a certain request, specified in the ar'''
+session expiration time, for a certain request, specified in the ar'''
 
     monitor = api.rate_limit_status()
     print json.dumps(monitor, sort_keys=True,
                     indent=4, separators=(',', ': '))
     return ()
+
+def twirp_main(api):
+    complete_mp_list = return_twitter_list()
+    fetched_mp_list = return_skip_list()
+
+    remaining_mp_list = list(set(complete_mp_list)-set(fetched_mp_list))
+
+    print len(remaining_mp_list)
+
+    for mp_tuple in remaining_mp_list:
+        try:
+            collect_twirp_data(api, mp_tuple[1], mp_tuple[0])
+        except:
+            print 'yo'
+
 
         
 class Tweet(object):
@@ -142,10 +167,24 @@ class Twirp(object):
 
 if __name__ == '__main__':
     to_do_list = return_twitter_list()
-    print to_do_list[0]
+    print to_do_list
     session_api = authorize_twitter()
     create_twirpy_db()
 
-    collect_twirp_data(session_api, 'KeeleyMP',1)
+    twirp_main(session_api)
     monitor_calls(session_api)
+
+
+
+# def pull_tweets_from_user(api, handle):
+#     print handle
+#     mp = api.get_user(screen_name=handle)
+#     for status in tweepy.Cursor(api.user_timeline, id=mp.id).items(1000):
+#         tweet_parser(status)
+
+#         for mention in status.entities['user_mentions']:
+#             print i, mention['screen_name'], '\t', status.text
+    
+#     time.sleep(45)
+
 

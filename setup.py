d87372cf27f6d71a6a0ba3d3901e2ec577251634
Eric Hambro <eric.hambro@gmail.com>
2015-11-28 02:17:39 +0000

    ready for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='archipelago',
+      version='0.1',
+      description='a tool for accessing publically available UK Government data on Members of Parliament (MPs)',
+      url='https://github.com/condnsdmatters/archipelago',
+      author='Eric Hambro',
+      author_email='ehambro@gmail.com',
+      license='MIT',
+      packages=['archipelago'],
+      zip_safe=False
+      )
